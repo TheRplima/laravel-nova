839296ae057b462ecc470588643685812a963358
Rodrigo Lima <rodrigolima@brasilcard.net>
2023-04-24 10:59:42 -0300

    feat: criação de nova dashboard para campanhas

---
+++ app/Nova/Dashboards/Campaings.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Nova\Dashboards;
+
+use Laravel\Nova\Dashboard;
+
+class Campaings extends Dashboard
+{
+    /**
+     * Get the cards for the dashboard.
+     *
+     * @return array
+     */
+    public function cards()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the URI key for the dashboard.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'campaings';
+    }
+
+    public function name() {
+        return 'Campanhas';
+    }
+}
